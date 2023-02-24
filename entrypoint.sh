1e12cbe7dc76252d23efdab83eac03bccb748a7d
JimTheBeam <24dmitriy@mail.ru>
2023-02-25 01:21:10 +0300

    [b] fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # make migrations
-goose -dir ./migrations postgres "postgres://postgres:postgres@db_postgresql:5432/psn_discount?sslmode=disable" up
+goose -dir ./migrations/postgres postgres "postgres://postgres:postgres@db_postgresql:5432/psn_discount?sslmode=disable" up
 
 # run the service
 ./psn_discounter -c config.yaml
